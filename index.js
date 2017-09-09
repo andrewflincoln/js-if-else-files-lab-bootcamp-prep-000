96feee8bfabe16dd868293f242734df9c96362d6
Andrew Lincoln <andrewjlincoln@gmail.com>
2017-09-09 21:23:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var name = "Susan"
 var height = 70
 
-if (name === "Susan") {
-  console.log("The name is correct")
-}
-else {
-  console.log("Name shoulda been Susan!")
-}
+//if (name === "Susan") {
+  //console.log("The name is correct")
+//else {
+  //console.log("Name shoulda been Susan!")
+//}
 
 
 // Don't worry about this
